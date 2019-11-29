eb14e994e29e06eddff2b58ad4e5f30fbe1ad7be
Omar Aflak <aflakomar@gmail.com>
2019-11-29 11:26:24 +0100

    added utils for coverage test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 var assert = require('assert');
+var sum = require('../utils/sum.js')
 
 describe('Some simple test', () => {
-    it('should return true', () => {
-        assert.strictEqual(true, true);
+    it('should return sum', () => {
+        assert.strictEqual(sum(1, 1), 2);
     });
 });
