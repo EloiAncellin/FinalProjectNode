98848ed235a70b7be0ca10b48bd05f234e4c6853
Omar Aflak <aflakomar@gmail.com>
2019-12-02 18:54:41 +0100

    update mongodb cred

--- test/test.js
+++ test/test.js
@@ -2,12 +2,10 @@ require('dotenv').config();
 const expect = require('chai').expect;
 const sum = require('../utils/sum.js');
 const MongoClient = require('mongodb').MongoClient;
+const Server = require('mongodb').Server;
 const host = process.env.DB_HOST_TEST;
 const port = process.env.DB_PORT_TEST;
-const user = process.env.DB_USER_TEST;
-const pass = process.env.DB_PASS_TEST;
 const name = process.env.DB_NAME_TEST;
-const url = `mongodb://${user}:${pass}@${host}:${port}/${name}`;
 
 describe('Some simple test', () => {
     it('should return sum', () => {
@@ -15,20 +13,19 @@ describe('Some simple test', () => {
     });
 });
 
-/*
 describe('Database testing', () => {
     it('should write and read successfully', () => {
-        MongoClient.connect(url, function(err, db) {
+        MongoClient.connect(new Server(host, port), function(err, client) {
             expect(err).to.not.exist;
-            var dbo = db.db(process.env.DB_TEST_NAME);
+            var db = client.db(name);
             
             // write
             var object = { 'name': 'omar aflak' };
-            dbo.collection('users').insertOne(object, function(err, result){
+            db.collection('users').insertOne(object, function(err, result){
                 expect(err).to.not.exist;
 
                 // read
-                dbo.collection('users').findOne({}, function(err, result){
+                db.collection('users').findOne({}, function(err, result){
                     expect(err).to.not.exist;
                     expect(result).to.equal(object);
                 });
@@ -36,4 +33,3 @@ describe('Database testing', () => {
         });
     });
 });
-*/
