0809a5efe0e1ff2d4ec2f775bad708907dff95d8
Omar Aflak <aflakomar@gmail.com>
2019-12-12 18:00:04 +0100

    used promises in tests

--- src/test/test.ts
+++ src/test/test.ts
@@ -14,43 +14,44 @@ describe('Database testing', () => {
     it('connect to database', (done) => {
         mongoUtils.connect().then((db) => {
             _db = db;
+            expect(db).to.not.null;
             done();
         }).catch(done);
     });
 
     it('clear database', (done) => {
         mongoUtils.clear().then((result) => {
+            expect(result).to.not.null;
             done();
         }).catch(done);
     });
 
     it('initialize database', (done) => {
         mongoUtils.init().then((result) => {
+            expect(result).to.not.null;
             done();
         }).catch(done);
     });
 
     it('write user to database', (done) => {
-        _db.collection('users').insertOne({ email: 'toto1@ece.fr' }, function(err, result) {
-            expect(err).to.not.exist;
+        _db.collection('users').insertOne({ email: 'toto1@ece.fr' }).then((result) => {
+            expect(result).to.not.null;
             done();
-        });
+        }).catch(done);
     });
 
     it('read user from database', (done) => {
-        _db.collection('users').findOne({ email: 'toto1@ece.fr' }, function(err, result) {
-            expect(err).to.not.exist;
+        _db.collection('users').findOne({ email: 'toto1@ece.fr' }).then((result) => {
             expect(result).to.not.null;
             done();
-        });
+        }).catch(done);
     });
 
     it('read non existant user from database', (done) => {
-        _db.collection('users').findOne({ email: 'xxx' }, function(err, result) {
-            expect(err).to.not.exist;
+        _db.collection('users').findOne({ email: 'xxx' }).then((result) => {
             expect(result).to.null;
             done();
-        });
+        }).catch(done);
     });
 
     it('close database', () => {
