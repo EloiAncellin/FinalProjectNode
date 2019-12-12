7f0afbd10aa75f110d3bbc5c87d70157e998e46c
Omar Aflak <aflakomar@gmail.com>
2019-12-12 13:21:51 +0100

    rm mock cod

--- src/views/home.ts
+++ src/views/home.ts
@@ -1,23 +1,8 @@
-import api from '../services/api/users';
-import User from '../models/user';
 const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    api.getUserByEmail('aflakomar@gmail.com').then((user) => {
-        res.json(user);
-    }).catch((error) => {
-        res.send(error);
-    });
-});
-
-router.get('/create', function (req, res) {
-    const user = new User(null, 'omaraflak@gmail.com', 'pass', 'Omar', 'Aflak');
-    api.createUser(user).then((user) => {
-        res.json(user);
-    }).catch((error) => {
-        res.send(error);
-    });
+    res.send('Home page.');
 });
 
 module.exports = router;
