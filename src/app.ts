e4fbe1192645b84e881a5120af70e5f668977eb2
Omar Aflak <aflakomar@gmail.com>
2019-12-12 17:45:05 +0100

    syntax..

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,7 @@ import mongoUtils from './utils/mongoUtils';
 const express = require('express');
 const app = express();
 
-mongoUtils.connect()
-.then(mongoUtils.clear)
-.then(mongoUtils.init)
-.then((res) => {
+mongoUtils.connect().then(mongoUtils.clear).then(mongoUtils.init).then((res) => {
     const web_port = process.env.WEB_PORT
     app.use('/', require('./views/home'));
     app.listen(web_port, () => console.log(`Server listening on port ${web_port}!`));
