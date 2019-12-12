d63f37a9ac5c94f8f2251c1da67fbd2d7d23bf20
Omar Aflak <aflakomar@gmail.com>
2019-12-12 17:46:45 +0100

    rm useless param

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import mongoUtils from './utils/mongoUtils';
 const express = require('express');
 const app = express();
 
-mongoUtils.connect().then(mongoUtils.clear).then(mongoUtils.init).then((res) => {
+mongoUtils.connect().then(mongoUtils.clear).then(mongoUtils.init).then(() => {
     const port = process.env.WEB_PORT
     app.use('/', require('./views/home'));
     app.listen(port, () => console.log(`Server listening on port ${port}!`));
