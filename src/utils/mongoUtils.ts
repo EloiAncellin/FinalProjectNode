5c892fb9a1448562af1c895b9c7343688410be5d
Omar Aflak <aflakomar@gmail.com>
2019-12-13 17:29:46 +0100

    use mongoose connection

--- src/utils/mongoUtils.ts
+++ src/utils/mongoUtils.ts
@@ -1,41 +1,18 @@
-const MongoClient = require('mongodb').MongoClient;
-const Server = require('mongodb').Server;
+const mongoose = require('mongoose');
 const host = process.env.DB_HOST;
 const port = process.env.DB_PORT;
 const name = process.env.DB_NAME;
 
-let _client;
 let _db;
 
 export = {
     connect: function() {
-        return new Promise((resolve, reject) => {
-            MongoClient.connect(new Server(host, port)).then((client) => {
-                _client = client;
-                _db = client.db(name);
-                resolve(_db);
-            }).catch(reject);
-        });
-    },
-    db: function() {
-        return _db;
-    },
-    close: function() {
-        _client.close();
+        return mongoose.connect(`mongodb://${host}:${port}/${name}`, { useNewUrlParser: true, useUnifiedTopology: true });
     },
     clear: function() {
-        return new Promise((resolve, reject) => {
-            _db.dropDatabase().then((result) => {
-                if (result) {
-                    resolve(true);
-                } else {
-                    reject('Could not drop database.');
-                }
-            }).catch(reject);
-        });
+        return mongoose.connection.db.dropDatabase();
     },
-    init: function() {
-        _db = _client.db(name);
-        return _db.collection('users').createIndex( { "email": 1 }, { unique: true } );
+    close: function() {
+        mongoose.connection.close();
     }
 };
