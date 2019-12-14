39e3cba6e895e8758f8e9cb39431aebacae91fb7
Omar Aflak <aflakomar@gmail.com>
2019-12-14 22:24:56 +0100

    different database for testing

--- src/utils/mongoUtils.ts
+++ src/utils/mongoUtils.ts
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const host = process.env.DB_HOST;
 const port = process.env.DB_PORT;
 const name = process.env.DB_NAME;
+const nameTest = process.env.DB_NAME_TEST;
 
 export = {
-    connect: function() {
-        return mongoose.connect(`mongodb://${host}:${port}/${name}`, {
+    connect: function(isTest) {
+        const db = isTest ? nameTest : name;
+        return mongoose.connect(`mongodb://${host}:${port}/${db}`, {
             useCreateIndex: true,
             useNewUrlParser: true,
             useUnifiedTopology: true
