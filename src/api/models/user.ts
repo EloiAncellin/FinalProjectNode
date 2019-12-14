12f68209be4a48395b593e9974a99525aee02876
Omar Aflak <aflakomar@gmail.com>
2019-12-14 01:01:02 +0100

    email unique

--- src/api/models/user.ts
+++ src/api/models/user.ts
@@ -7,6 +7,8 @@ const userSchema = new mongoose.Schema({
         type: String,
         trim: true,
         required: true,
+        unique: true,
+        lowercase: true
     },
     password: {
         type: String,
