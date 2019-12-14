18533c78b8919ac0b2adc09b97960c88c842a2e0
Omar Aflak <aflakomar@gmail.com>
2019-12-14 21:54:35 +0100

    create multiple metrics at a time

--- src/api/controllers/metric.ts
+++ src/api/controllers/metric.ts
@@ -3,13 +3,16 @@ const Metric = require('../models/metric');
 
 export = {
     create: function(req, res) {
-        Metric.create({
-            userId: req.body.user._id,
-            name: req.body.name,
-            value: req.body.value,
-            date: new Date()
-        }).then((doc) => {
-            res.status(200).json(new Response(Response.SUCCESS, doc));
+        const metrics = req.body.metrics.map((metric) => {
+            return {
+                userId: req.body.user._id,
+                name: metric.name,
+                value: metric.value,
+                date: new Date()
+            }
+        });
+        Metric.create(metrics).then((docs) => {
+            res.status(200).json(new Response(Response.SUCCESS, docs));
         }).catch((err) => {
             res.status(400).json(new Response(Response.ERROR, err));
         });
