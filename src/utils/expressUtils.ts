2eec8e2640ba006fc17c88e6bc4545e46f8bf8d3
Omar Aflak <aflakomar@gmail.com>
2019-12-15 11:54:37 +0100

    cleaned

--- src/utils/expressUtils.ts
+++ src/utils/expressUtils.ts
@@ -3,23 +3,25 @@ const express = require('express');
 const colors = require('colors/safe');
 const app = express();
 
-colors.enable();
 let _server;
 
 const cheezyLog = () => {
-    const logs = [
-        colors.brightMagenta.bold('===  cheezy log activated  ==='),
-        colors.brightGreen.bold('Congratulation! You made it so far, wow! The server is now up and running :)'),
-        colors.brightYellow.underline(`http://localhost:${_server.address().port}`),
-        colors.bold('Please rate this project 💯'),
-        colors.brightCyan.bold('Goodbye friend.'),
-        colors.brightMagenta.bold('=== cheezy log deactivated ===')
-    ]
-    console.log(logs.join('\n'));
-}
+    if (process.env.VERBOSE == '1') {
+        const logs = [
+            colors.brightMagenta.bold('===  cheezy log activated  ==='),
+            colors.brightGreen.bold('Congratulation! You made it so far, wow! The server is now up and running :)'),
+            colors.brightYellow.underline(`http://localhost:${_server.address().port}`),
+            colors.bold('Please rate this project 💯'),
+            colors.brightCyan.bold('Goodbye friend.'),
+            colors.brightMagenta.bold('=== cheezy log deactivated ===')
+        ]
+        console.log(logs.join('\n'));
+    }
+};
 
 export = {
     start: (isTest) => {
+        colors.enable();
         return new Promise((resolve, reject) => {
             app.use(bodyParser.urlencoded({ extended: false }));
             app.use(bodyParser.json());
@@ -32,9 +34,7 @@ export = {
             const _port = isTest ? portTest : port;
 
             _server = app.listen(_port, () => {
-                if (process.env.VERBOSE == '1') {
-                    cheezyLog();
-                }
+                cheezyLog();
                 resolve(app);
             });
         });
