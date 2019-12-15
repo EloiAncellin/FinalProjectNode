0a1054983e785c2675bfb7d080ab845a243b0abc
Omar Aflak <aflakomar@gmail.com>
2019-12-15 15:55:22 +0100

    logout

--- src/views/home.ts
+++ src/views/home.ts
@@ -12,6 +12,11 @@ router.get('/hello', auth.checkCookies, auth.ensureAuthenticated, (req, res) => 
     res.send('Secure page.');
 });
 
+router.get('/bye', auth.checkCookies, auth.ensureAuthenticated, (req, res) => {
+    res.clearCookie('jwt');
+    res.send('Goodbye.');
+});
+
 router.get('/auth', (req, res) => {
     const cred = {
         email: 'toto01@ece.fr',
@@ -21,7 +26,8 @@ router.get('/auth', (req, res) => {
         if (response.status === Response.SUCCESS) {
             res.cookie('jwt', response.result.token, {
                 httpOnly: true,
-                secure: false // no ssl for now...
+                secure: false, // no ssl for now...
+                maxAge: 60 * 60 * 24 * 7
             });
         }
         res.status(response.code).json(response);
