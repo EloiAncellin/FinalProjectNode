555e926c9ec6469c110cc95916a02bc26ea481c3
Omar Aflak <aflakomar@gmail.com>
2019-12-15 21:40:22 +0100

    metric view

---
+++ src/views/public/metric/index.js
@@ -0,0 +1,66 @@
+let token;
+
+const onLoaded = () => {
+    token = localStorage.getItem('jwt') || '';
+    if (token === ''){
+        window.location.replace('/login');
+    }
+};
+
+const getMetrics = async (metricName) => {
+    const request = new Request(`/api/metrics/collection/${metricName}`);
+    const response = await fetch(request, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'authorization': token
+        }
+    });
+
+    const res = JSON.parse(await response.text());
+    if (res.status === 'success') {
+        displayMetrics(metricName, res.result);
+    } else {
+        alert(JSON.stringify(res.result));
+    }
+};
+
+const displayMetrics = (metricName, metrics) => {
+    const data = metrics.map((metric) => {
+        return {
+            x: metric.date,
+            y: metric.value
+        };
+    });
+
+    const serie = {
+        label: metricName,
+        borderColor: 'blue',
+        data: data
+    };
+
+    const ctx = document.getElementById('myChart');
+    const lines = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [serie]
+        },
+        options: {
+            elements: {
+                line: {
+                    tension: 0
+                }
+            },
+            scales: {
+                xAxes: [{
+                    type: 'time'
+                }]
+            }
+        }
+    });
+};
+
+const onDisconnectClicked = () => {
+    localStorage.setItem('jwt', '');
+    window.location.replace('/login');
+};
