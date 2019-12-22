c6aff6fc0ce04c2ea28562cc8c87a6d4d42a800d
Eloi Ancellin <eloi.ancellin@gmail.com>
2019-12-22 11:44:02 +0100

    Linking api with add metric, still a bug

--- src/views/public/addMetric/index.js
+++ src/views/public/addMetric/index.js
@@ -25,40 +25,7 @@ const getMetrics = async (metricName) => {
     }
 };
 
-const displayMetrics = (metricName, metrics) => {
-    const ctx = document.getElementById('myChart');
-    const lines = new Chart(ctx, {
-        type: 'line',
-        data: {
-            datasets: [
-                {
-                    label: metricName,
-                    borderColor: 'blue',
-                    data: metrics.map((metric) => {
-                        return {
-                            x: metric.date,
-                            y: metric.value
-                        };
-                    })
-                }
-            ]
-        },
-        options: {
-            maintainAspectRatio: false,
-            elements: {
-                line: {
-                    tension: 0.2,
-                    borderWidth: 1.5
-                }
-            },
-            scales: {
-                xAxes: [{
-                    type: 'time'
-                }]
-            }
-        }
-    });
-};
+
 
 const onDisconnectClicked = () => {
     localStorage.setItem('jwt', '');
@@ -67,33 +34,27 @@ const onDisconnectClicked = () => {
 
 const onHomeClicked = () => {
     window.location.replace('/');
-}
-
-
-
-
-
-
+};
 
 
 const onAddClicked = async () => {
+	console.log(token);
     const metricName = document.getElementById('metric_name').value;
     const metricValue = document.getElementById('metric_value').value;
    
-    const request = new Request('/api/users/register');
+    const request = new Request('/api/metrics');
     const response = await fetch(request, {
         method: 'POST',
         body: JSON.stringify({
-            email: email,
-            password: password,
-            firstName: firstName,
+            Token: token,
+            name : metricName,
+            value: metricValue,
         }),
         headers: { 'Content-Type': 'application/json' }
     });
-    
     const res = JSON.parse(await response.text());
     if (res.status === 'success') {
-        window.location.replace('/login');
+        window.location.replace('/home');
     } else {
         toastr.error(res.result);
     }
